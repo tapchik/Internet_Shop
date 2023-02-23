e85fd944c9e1295b909d74f46dde0b7aab7271c8
Max <44063879+tapchik@users.noreply.github.com>
2023-02-23 22:52:03 +0500

    Working on cart counter

--- web/src/test/java/Testing.java
+++ web/src/test/java/Testing.java
@@ -7,7 +7,10 @@ public class Testing {
 
     public void Test1() {
         Controller controller = new Controller();
-        //ArrayList<Product> result1 = controller.productRepository.getProducts("none", "description-desc");
+        ArrayList<Product> result1 = controller.productRepository.getProducts("none", "description-desc");
+        for (Product product: result1) {
+            System.out.println(product.getDescription());
+        }
     }
 
 }
