393ff45915dd65c4ca7a21038356f3ba418569af
Max <44063879+tapchik@users.noreply.github.com>
2023-02-23 20:23:27 +0500

    For testing

---
+++ web/src/test/java/Testing.java
@@ -0,0 +1,13 @@
+import com.example.springwebapp.controller.Controller;
+import com.example.springwebapp.model.Product;
+
+import java.util.ArrayList;
+
+public class Testing {
+
+    public void Test1() {
+        Controller controller = new Controller();
+        //ArrayList<Product> result1 = controller.productRepository.getProducts("none", "description-desc");
+    }
+
+}
