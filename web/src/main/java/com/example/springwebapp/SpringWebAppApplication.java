444cc2feb5bc0a7f60503f39a9fb603d55f91548
pashcash <115349015+pashcash@users.noreply.github.com>
2023-02-23 12:53:55 +0500

    Add fragments, replace table with list of products

--- web/src/main/java/com/example/springwebapp/SpringWebAppApplication.java
+++ web/src/main/java/com/example/springwebapp/SpringWebAppApplication.java
@@ -10,7 +10,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class })
-public class SpringWebAppApplication {
+public class SpringWebAppApplication
+{
 
     public static void main(String[] args) {
         SpringApplication.run(SpringWebAppApplication.class, args);
