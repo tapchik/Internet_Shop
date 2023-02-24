afdab95af7ad7f5e69583be18ba7d1f8b13ee5e0
Max <44063879+tapchik@users.noreply.github.com>
2023-02-24 19:04:48 +0500

    Moved to fragments

--- web/src/test/java/Testing.java
+++ web/src/test/java/Testing.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 public class Testing {
 
     public void Test1() {
-        Controller controller = new Controller();
-        ArrayList<Product> result1 = controller.productRepository.getProducts("none", "description-desc");
-        for (Product product: result1) {
-            System.out.println(product.getDescription());
-        }
+        //Controller controller = new Controller();
+        //ArrayList<Product> result1 = controller.productRepository.getProducts("none", "description-desc");
+        //for (Product product: result1) {
+        //    System.out.println(product.getDescription());
+        //}
     }
 
 }
