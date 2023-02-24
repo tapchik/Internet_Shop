55f987767e61132864e33273dab9c3eaf3a4e8e7
Max <44063879+tapchik@users.noreply.github.com>
2023-02-24 19:06:01 +0500

    Products added to cart stored in session

--- web/src/main/java/com/example/springwebapp/controller/Controller.java
+++ web/src/main/java/com/example/springwebapp/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.example.springwebapp.controller;
 
 import com.example.springwebapp.model.Product;
+import com.example.springwebapp.repository.CartRepository;
 import com.example.springwebapp.repository.ProductRepository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,13 @@ import javax.servlet.http.HttpSession;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    public ProductRepository productRepository;
+    private ProductRepository productRepository;
+    private CartRepository cartRepository;
     private int temporary_count;
 
     public Controller() {
         productRepository = new ProductRepository();
+        cartRepository = new CartRepository();
         temporary_count = 0;
     }
 
@@ -34,13 +37,19 @@ public class Controller {
                                   @RequestParam(required=false) String filter,
                                   @RequestParam(name="sort-by", defaultValue = "none") String sort_by
                                   ) {
-        String sid = session.getId();
-        model.addAttribute("sid", sid);
+        // adding attribute "products"
         ArrayList<Product> products = productRepository.getProducts(filter=filter,sort_by=sort_by);
         model.addAttribute("products", products);
-        String items_in_cart = String.valueOf(temporary_count); //TODO count real number of items in cart
-        model.addAttribute("items_in_cart", items_in_cart);
         model.addAttribute("filter", filter);
+        // section adding attribute "items_in_cart"
+        int value = cartRepository.countItemsInCart(session);
+        String items_in_cart = String.valueOf(value);
+            //String items_in_cart = String.valueOf(temporary_count); //TODO count real number of items in cart
+        model.addAttribute("items_in_cart", items_in_cart);
+        // adding attribute "sid"
+        String sid = session.getId();
+        model.addAttribute("sid", sid);
+        // returning index.html
         return "index";
     }
 
@@ -61,8 +70,11 @@ public class Controller {
     @RequestMapping("/add_to_cart")
     public String addToCart(Model model, HttpSession session, @RequestParam(name="product_id") String product_id) {
         String sid = session.getId();
-        this.temporary_count += 1;
-        String items_in_cart = String.valueOf(temporary_count); //TODO count real number of items in cart
+        cartRepository.addProductToCart(session, product_id);
+            //this.temporary_count += 1;
+            //String items_in_cart = String.valueOf(temporary_count); //TODO count real number of items in cart
+        int value = cartRepository.countItemsInCart(session);
+        String items_in_cart = String.valueOf(value);
         model.addAttribute("items_in_cart", items_in_cart);
         return "fragments/cart-counter";
     }
