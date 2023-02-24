1bf1ad333e271103a385400894e4ac6bf02965a6
pashcash <115349015+pashcash@users.noreply.github.com>
2023-02-24 14:19:56 +0500

    Fix

--- web/src/main/java/com/example/springwebapp/repository/CartRepository.java
+++ web/src/main/java/com/example/springwebapp/repository/CartRepository.java
@@ -14,15 +14,23 @@ public class CartRepository {
         carts = new HashMap<HttpSession, Cart>();
     }
 
+    public void addCartCheck(HttpSession session) {
+        if (!this.carts.containsKey(session))
+            carts.put(session, new Cart());
+    }
+
     public void addProductToCart(HttpSession session, String product_id) {
+        this.addCartCheck(session);
         carts.get(session).addToCart(product_id);
     }
 
     public Cart getCart(HttpSession session) {
+        this.addCartCheck(session);
         return carts.get(session);
     }
 
     public int countItemsInCart (HttpSession session) {
+        this.addCartCheck(session);
         return this.getCart(session).countOfAllProducts();
     }
 }
